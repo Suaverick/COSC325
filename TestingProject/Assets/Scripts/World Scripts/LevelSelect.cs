797317ec06f655d63151e4bb1499538050522134
Rahsaan0 <113149633+Rahsaan0@users.noreply.github.com>
2022-12-09 13:16:28 -0500

    Level Select
    Adds Level Select scene

---
+++ TestingProject/Assets/Scripts/World Scripts/LevelSelect.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelSelect : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public string level;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+  public void OpenScene()
+    {
+        SceneManager.LoadScene(level);
+    }
+}
